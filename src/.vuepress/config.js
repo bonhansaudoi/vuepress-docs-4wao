bb8328db49b642ace5b7f4bcfc5c907ba9200ec3
bonhansaudoi <31092553+bonhansaudoi@users.noreply.github.com>
2021-03-21 12:53:06 +0700

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -29,7 +29,7 @@ module.exports = {
       },
       {   
         text: 'SadMiss',
-        link: 'https://bonhansaudoi.github.io/sadmiss/'
+        link: 'https://bonhansaudoi.github.io/sadmiss/myMiss.html'
       },
     ],
     sidebar: {
